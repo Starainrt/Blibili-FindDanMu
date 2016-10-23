e914e747d80da279a3f823d2afb897252027d6af
Twilightstar <admin@rainrt.com>
2016-10-23 12:05:21 +0800

    Change CSS

--- byuser.php
+++ byuser.php
@@ -6,6 +6,11 @@
 <meta name="referrer" content="origin-when-cross-origin"/>
 <title>Bibili弹幕发送查询</title>
 <style>
+body {
+				background-image:url('nanami.jpg');
+				background-position:top;
+				background-repeat:no-repeat;
+			}
 h2{
 				font-family: '华文楷体';
 				font-size: 29px;
@@ -41,22 +46,33 @@ h2{
 			-moz-box-shadow: 0 1px 8px 2px rgba(0, 0, 0, 0.3);
 			box-shadow: 0 1px 8px 2px rgba(0, 0, 0, 0.3);
 		}
+		div.transbox
+	{
+		margin:30px;
+		background-color: #ffffff;
+		/* border: 1px solid black; */
+		/* for IE */
+		filter:alpha(opacity=60);
+		/* CSS3 standard */
+		opacity:0.8;
+	}
 	</style>
 </head>
 <body>
 <script src="lib/functionuser.js" ></script>
 <form id="send" method="get" >
 <center>
+<div class="transbox">
 <tr>
-<p>视频地址或AV号:分P号</p>
+<p><b>视频地址或AV号:分P号</b></p>
 <input id="url" name="url" type="text" size=38 "/>
 </tr>
 <tr>
-<p>用户mid</p>
+<p><b>用户mid</b></p>
 <input id="danmu" type="text" size=38  />
 </tr>
 <br />
-<p style="font-size: 16px;color:blue;"><input type="checkbox" id="reg" name="reg" value=1 />使用哈希值</p>
+<p style="font-size: 16px;color:blue;"><input type="checkbox" id="reg" name="reg" value=1 /><b>使用哈希值</b></p>
 <tr>
 <input type="button" class="button" id="find" name="find" value="查找" onclick="sendquery()"/>
 <input type="button" class="button" id="btnsetsumei"  value="说明" onclick="setsumei()"/>
@@ -64,7 +80,7 @@ h2{
 <noscript>
 <p>您的电脑不支持Javascript，本站功能您无法使用。请谅解</p>
 </noscript>
-<p id="setsumei" style="font-size: 16px; color:blue;"></p>
-<p id="show" style="font-size: 16px;"></p>
+<b><p id="setsumei" style="font-size: 16px; color:blue;"></p></b>
+<b><p id="show" style="font-size: 16px;"></p></b>
 </center>
 </form>
